e247777b464f9862f67d7f8aabb3772de9c5d156
Collin Simler <simler.collin@gmail.com>
2023-02-01 21:15:20 -0600

    added to basicArrays

--- BasicArrays/BasicArrays/BasicArrays.cs
+++ BasicArrays/BasicArrays/BasicArrays.cs
@@ -87,5 +87,90 @@
             return nums;
         }
 
+        /// <summary>
+        /// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+        /// </summary>
+        /// <param name="arr"></param>
+        public static int[] DuplicateZeros(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == 0 && i != arr.Length -1)
+                {
+                    arr = ShiftToRight(arr, i);
+                    arr[i+1] = 0;
+                    i++;
+                }
+            }
+
+            return arr;
+
+        }
+
+        public static int[] ShiftToRight(int[] arr, int index)
+        {
+            for(var i = arr.Length-2; i > index; i--)
+            {
+                arr[i+1] = arr[i];
+
+            }
+
+            return arr;
+        }
+
+        /// <summary>
+        /// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="m"></param>
+        /// <param name="nums2"></param>
+        /// <param name="n"></param>
+        public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] mergedArrays = new int[n+m];
+
+            int nums1Index = 0;
+            int nums2Index = 0;
+            int mergedIndex = 0;
+
+            while(n > 0 || m > 0)
+            {
+                if (nums1[nums1Index] < nums2[nums2Index] && m > 0)
+                {
+                    mergedArrays[mergedIndex] = nums1[nums1Index];
+                    m--;
+                    mergedIndex++;
+                    nums1Index++;
+
+                }
+                else if (nums2[nums2Index] < nums1[nums1Index] && n > 0)
+                {
+                    mergedArrays[mergedIndex] = nums2[nums2Index];
+                    n--;
+                    mergedIndex++;
+                    nums2Index++;
+                }
+                else if (m > 0)
+                {
+                    mergedArrays[mergedIndex] = nums1[nums1Index];
+                    m--;
+                    mergedIndex++;
+                    nums1Index++;
+
+                }
+                else if (n > 0)
+                {
+                    mergedArrays[mergedIndex] = nums2[nums2Index];
+                    n--;
+                    mergedIndex++;
+                    nums2Index++;
+
+                }
+            }
+
+            
+            return mergedArrays;
+
+        }
     }
 }
--- BasicArrays/BasicArraysTest/BasicArrayTests.cs
+++ BasicArrays/BasicArraysTest/BasicArrayTests.cs
@@ -31,5 +31,35 @@ namespace BasicArraysTest
             Assert.AreEqual(2, BasicArrays.BasicArrays.SortedSquares(array));
 
         }
+
+        
+
+        [TestMethod]
+        public void TestDuplicateZeros()
+        {
+            int[] array = { 1,0,2,3,0,4,5,0 };
+
+            int[] result = BasicArrays.BasicArrays.DuplicateZeros(array);
+
+            Assert.AreEqual(true, result.SequenceEqual(new int[] { 1, 0, 0, 2, 3, 0, 0, 4 }));
+
+        }
+
+        [TestMethod]
+        public void TestMerge()
+        {
+            int[] array1 = { 1, 2, 3, 0, 0, 0 };
+
+            int m = 3;
+
+            int n = 3;
+
+            int[] array2 = { 2, 5, 6 };
+
+            int[] mergedResult = BasicArrays.BasicArrays.Merge(array1, m , array2, n);
+
+            Assert.AreEqual(true, mergedResult.SequenceEqual(new int[] { 1, 2, 2, 3, 5, 6 }));
+
+        }
     }
 }
