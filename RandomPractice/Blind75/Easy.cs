9ed20ba63d04845ce5813e1bf9eeb807c7e227aa
Collin Simler <Collin.Simler@sccompanies.com>
2023-03-29 07:59:06 -0500

    added more

--- RandomPractice/Blind75/Easy.cs
+++ RandomPractice/Blind75/Easy.cs
@@ -24,5 +24,91 @@
             //return 0 if none found
             return 0;
         }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int,int> numberCounts = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (numberCounts.ContainsKey(num))
+                    return true;
+
+                numberCounts.Add(num,1);
+            }
+
+            return false;
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+
+            Dictionary<char, int> characterCountsS = new Dictionary<char, int>();
+
+            foreach (var character in s)
+            {
+                if (characterCountsS.ContainsKey(character))
+                {
+                    characterCountsS[character]++;
+                }
+                else
+                {
+                    characterCountsS.Add(character,1);
+                }
+            }
+
+            foreach (char c in t)
+            {
+                if (characterCountsS.ContainsKey(c))
+                {
+                    characterCountsS[c]--;
+                    
+                }
+                else
+                {
+                    return false;
+                }
+
+                if (characterCountsS[c] < 0)
+                    return false;
+
+            }
+
+            foreach (var i in characterCountsS)
+            {
+                if (i.Value > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> indices = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int difference = target - nums[i];
+
+                if(indices.ContainsKey(difference))
+                    return new int[] { indices[difference], i };
+            }
+
+            return null;
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+
+        }
+
+
+
+
+
     }
 }
--- RandomPractice/Blind75Tests/EasyTests.cs
+++ RandomPractice/Blind75Tests/EasyTests.cs
@@ -17,5 +17,64 @@ namespace Blind75Tests
             Assert.AreEqual(8, Easy.MissingNumber(case0));
 
         }
+
+        [TestMethod]
+        public void TestContainsDuplicate()
+        {
+            int[] testCase = new[] { 1, 2, 3, 1 };
+
+            Assert.IsTrue(Easy.ContainsDuplicate(testCase));
+
+            testCase = new[] { 1, 2, 3, 4 };
+
+            Assert.IsFalse(Easy.ContainsDuplicate(testCase));
+
+        }
+
+
+        [TestMethod]
+        public void TestIsAnagram()
+        {
+            string word = "worddog";
+            string possibleAnagram = "goddrow";
+
+            Assert.IsTrue(Easy.IsAnagram(word,possibleAnagram));
+
+            word = "idontknow";
+            possibleAnagram = "notidk";
+
+            Assert.IsFalse(Easy.IsAnagram(word,possibleAnagram));
+
+        }
+
+        [TestMethod]
+        public void TestTwoSum()
+        {
+            int[] testCase = new[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            Assert.AreEqual(new int[]{0,1},Easy.TwoSum(testCase,target));
+
+            testCase = new[] { 3, 2, 4 };
+            target = 6;
+
+            Assert.AreEqual(new int[]{0,1},Easy.TwoSum(testCase,target));
+
+        }
+
+        [TestMethod]
+        public void TestIsAnagram()
+        {
+            string word = "worddog";
+            string possibleAnagram = "goddrow";
+
+            Assert.IsTrue(Easy.IsAnagram(word, possibleAnagram));
+
+            word = "idontknow";
+            possibleAnagram = "notidk";
+
+            Assert.IsFalse(Easy.IsAnagram(word, possibleAnagram));
+
+        }
     }
 }
